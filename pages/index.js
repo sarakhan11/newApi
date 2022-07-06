59daa53878d95dbc4932f1d67ebbf35534444f71
zeshan-alphasquad <zeshan.alphasquad@gmail.com>
2022-07-06 11:59:49 +0500

    2nd

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,28 @@
 
+import { NextFetchEvent } from "next/server";
 
-import react from "react";
-import HomePage from "../components/form/index";
+export const getstaticprops = async ()  => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
 
-export default function Home() {
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+const components = ({data}) =>{
   return (
-     <HomePage />
+    <>
+      
+      {data.map((curElem) => {
+        return <div key={curElem.id}>
+          <h3>{curElem.id}</h3>
+          <h2>{curElem.title}</h2>
+        </div>;
+      })}
+    </>
   );
-}
+};
+export default components;
