52b742386600f54b68032cc4a2471f101b33748d
zeshan-alphasquad <zeshan.alphasquad@gmail.com>
2022-07-07 00:16:56 +0500

    mynewcommit

--- pages/apii.js
+++
@@ -1,9 +0,0 @@
-import Api from "../components/callapi/index";
-
-export default function Form() {
-  return (
-    <>
-      <Api />
-    </>
-  );
-}
--- pages/new-form.js
+++
@@ -1,9 +0,0 @@
-import Form2 from "../components/Formnew/index";
-
-export default function form() {
-  return (
-    <>
-      <new-form />
-    </>
-  );
-}
